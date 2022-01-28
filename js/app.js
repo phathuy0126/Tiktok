dc2f2e64dd7e0ff7e7f422d01de09b0bf746c0f8
phathuy0126 <phathuy0126@gmail.com>
2022-01-28 14:36:15 +0700

    up code

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
 "use strict";
 const $ = document.querySelector.bind(document);
-const container = $('.containerScroll');
-const video = $('.video video');
+const $$ = document.querySelectorAll.bind(document);
+const containerScroll = $('.containerScroll');
 const navbar = $('.navbar');
-const app = {         
+
+const app = {
     tiktokPost: [
+        {
+            name: 'Phát Huy',
+            title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+            src: 'traval.mp4',
+            avatar: 'https://yt3.ggpht.com/ytc/AAUvwngVidpfS3gfW_OycU0IsosjQurTmGYk34Cb9iJD=s900-c-k-c0x00ffffff-no-rj',
+            nameMusic: 'nhạc nền: Nhạc này hơi chill',
+            avatarMusic: 'https://yt3.ggpht.com/ytc/AAUvwngVidpfS3gfW_OycU0IsosjQurTmGYk34Cb9iJD=s900-c-k-c0x00ffffff-no-rj'
+        },
+        {
+            name: 'Phát Huy',
+            title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+            src: 'hotgirl.mp4',
+            avatar: 'https://yt3.ggpht.com/ytc/AAUvwngVidpfS3gfW_OycU0IsosjQurTmGYk34Cb9iJD=s900-c-k-c0x00ffffff-no-rj',
+            nameMusic: 'nhạc nền: Nhạc này hơi chill',
+            avatarMusic: 'https://yt3.ggpht.com/ytc/AAUvwngVidpfS3gfW_OycU0IsosjQurTmGYk34Cb9iJD=s900-c-k-c0x00ffffff-no-rj'
+        },
         {
             name: 'Phát Huy',
             title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
@@ -23,14 +40,14 @@ const app = {
         }
     ],
     renderPost: function () {
-        const htmls = this.tiktokPost.map(function (post) {
+        const htmls = this.tiktokPost.map(function (post,index) {
             return `
                 <div class="containerItem">
                     <div class="video">
-                        <video src="video/${post.src}"></video>
+                        <video src="video/${post.src}" data-indexPost=${index}></video>
                     </div>
                     <div class="interact">
-                        <div class="avatar">
+                        <div class="interactItem avatar">
                             <div class="img">
                                 <img src=${post.avatar} alt="">
                             </div>                        
@@ -38,15 +55,15 @@ const app = {
                                 <i class="bx bx-plus"></i>
                             </div>
                         </div>
-                        <div class="heart">
+                        <div class="interactItem heart">
                             <i class="bx bxs-heart"></i>
                             <p>20.2k</p>
                         </div>
-                        <div class="comment">
+                        <div class="interactItem comment">
                             <i class="bx bx-message-rounded-dots"></i>
                             <p>215</p>
                         </div>
-                        <div class="share">
+                        <div class="interactItem share">
                             <i class="bx bxs-share"></i>
                             <p>29</p>
                         </div>
@@ -66,16 +83,30 @@ const app = {
                 `
         });
         const html = htmls.join("");
-        container.innerHTML = html;
+        containerScroll.innerHTML = html;
     },
-    playPause: function () {
-        // video.onclick = function (e) {
-        //     console.log(e.target);
-        // };
-    },    
+    handleEvent: function () {
+        const video = $$('.video video');
+
+        let indexPost = 0;        
+        // containerScroll.onscroll = function () {
+        //     const distanceTop = containerScroll.scrollTop  
+        //     console.log(distanceTop);               
+        // }
+
+        video.forEach(ele => {            
+            const rectTop = ele.getBoundingClientRect().top;
+            ele.onclick = function (e) {
+                indexPost = e.target.dataset.indexpost;
+                indexPost != e.target.dataset.indexpost ? ele.pause() : video[indexPost].play()                
+                console.log(e.target.dataset.indexpost);
+            }            
+            console.log(ele.offsetHeight)            
+        });
+    },
     start: function () {
         this.renderPost();
-        this.playPause();
+        this.handleEvent();
     }
 }
 app.start();
